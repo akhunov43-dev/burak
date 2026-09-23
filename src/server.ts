b9dc9461a38ef1b9b6dc2c2e8b4c30bacc6ab5d8
Kai <akhunov43@gmail.com>
2026-09-23 18:51:23 +0900

    feat: express integration

--- src/server.ts
+++ src/server.ts
@@ -15,8 +15,7 @@ const count: number = 100;
 // Design pattern: Middleware, Decotar
 
 
-import dotenv from 'dotenv'
-dotenv.config();
+
 
 /*
 console.log("PORT:", process.env.PORT);
@@ -24,14 +23,19 @@ console.log("PORT:", process.env.PORT);
 console.log("MONGO_URL:", process.env.PORT);
 
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT */
-
+import dotenv from 'dotenv'
+dotenv.config();
 import mongoose from 'mongoose';
+import app from "./app";
 
 mongoose
 .connect(process.env.MONGO_URL as string, {})
 .then((data) => {
     console.log("MongoDB connection succed");
     const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function() {
+        console.log(`The server is running succesfully on port: ${PORT}`);
+    });
 })
 .catch((err) => console.log("ERROR on connection MongpDB", err));
 
