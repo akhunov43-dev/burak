451347975962b487a156555e09debae331892f57
Kai <akhunov43@gmail.com>
2026-09-23 18:26:45 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,24 @@ const count: number = 100;
 
 // Design pattern: Middleware, Decotar
 
+
 import dotenv from 'dotenv'
 dotenv.config();
 
+/*
 console.log("PORT:", process.env.PORT);
 
 console.log("MONGO_URL:", process.env.PORT);
 
-// CLUSTER => DATABASE => COLLECTION => DOCUMENT
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT */
+
+import mongoose from 'mongoose';
+
+mongoose
+.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+    console.log("MongoDB connection succed");
+    const PORT = process.env.PORT ?? 3003;
+})
+.catch((err) => console.log("ERROR on connection MongpDB", err));
+
